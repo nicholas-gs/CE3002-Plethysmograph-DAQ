a407602512d6996ed0f949cd935730a242487e39
nicholasgan <nicholasganshyan@gmail.com>
2020-09-27 01:00:19 +0800

    add project definitions

---
+++ Plethysmograph-DAQ/lib/Definitions/Definitions.h
@@ -0,0 +1,17 @@
+// Project Definitions
+#define ADC_SIMPLE     0
+#define ADC_OVERSAMPLE 1
+#define DEBUG_ON       1
+#define DEBUG_OFF      0
+
+#define SERIAL_BAUD_RATE        9600
+#define ESP8266_BAUD_RATE       115200
+#define ESP_SERIAL_RECEIVE_PIN  5
+#define ESP_SERIAL_TRANSMIT_PIN 6
+
+#define ADC_REF   5
+#define ADC_STEPS 4096
+
+// Project Configurations
+#define ADC_MODE   ADC_SIMPLE
+#define DEBUG_MODE DEBUG_ON
