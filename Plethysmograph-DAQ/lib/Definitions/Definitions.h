34f32e7b78730c10bb40ca5dd3410e961dcabbaa
nicholasgan <nicholasganshyan@gmail.com>
2020-10-07 19:18:27 +0800

    add ewma and wifi definitions

--- Plethysmograph-DAQ/lib/Definitions/Definitions.h
+++ Plethysmograph-DAQ/lib/Definitions/Definitions.h
@@ -1,8 +1,11 @@
 // Project Definitions
-#define ADC_SIMPLE     0
-#define ADC_OVERSAMPLE 1
-#define DEBUG_ON       1
-#define DEBUG_OFF      0
+#define ADC_SIMPLE   0
+#define ADC_AVERAGE  1
+#define ADC_EWMA     2
+#define DEBUG_ON     1
+#define DEBUG_OFF    0
+#define WIFI_PRESENT 1
+#define WIFI_ABSENT  0
 
 #define SERIAL_BAUD_RATE        9600
 #define ESP8266_BAUD_RATE       9600
@@ -13,8 +16,9 @@
 #define ADC_STEPS 4096
 
 // Project Configurations
-#define ADC_MODE   ADC_OVERSAMPLE
-#define DEBUG_MODE DEBUG_ON
+#define ADC_MODE      ADC_EWMA
+#define DEBUG_MODE    DEBUG_ON
+#define WIRELESS_MODE WIFI_ABSENT
 
 // Pins
 #define LED_PIN    11
